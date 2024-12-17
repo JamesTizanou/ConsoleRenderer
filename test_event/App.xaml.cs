cd75d11ffe5c3dd4aff51a4594f0a81220e06d2f
JamesTizanou <jamestizanou@gmail.com>
2024-12-16 20:46:17 -0500

    jnn

---
+++ test_event/App.xaml.cs
@@ -0,0 +1,16 @@
+﻿using System.Configuration;
+using System.Data;
+using System.Security.Cryptography.X509Certificates;
+using System.Windows;
+
+namespace test_event
+{
+    /// <summary>
+    /// Interaction logic for App.xaml
+    /// </summary>
+    public partial class App : Application
+    {
+        public static Dictionary<string, Window> Windows = new Dictionary<string, Window>();
+    }
+
+}
---
+++ test_event/test.xaml.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace test_event
+{
+    /// <summary>
+    /// Interaction logic for test.xaml
+    /// </summary>
+    public partial class Test : Window
+    {
+        public Test()
+        {
+            InitializeComponent();
+            if (!App.Windows.ContainsKey("test"))
+            {
+                App.Windows.Add("test", this);
+            }
+            
+            //ValueChanged = onValueChange;
+            //Console.WriteLine("Value: " + Value.Text);
+            //Value.TextChanged += onChangeText;
+            //btnAff.Click += BtnAff_Click;
+            if (App.Windows.ContainsKey("main"))
+            {
+                ((MainWindow)App.Windows["main"]).ValueChanged += Test_ValueChanged;
+            }
+            
+        }
+
+        private void Test_ValueChanged(string obj)
+        {
+            this.Value.Text = obj;
+        }
+
+        private void BtnAff_Click(object sender, RoutedEventArgs e)
+        {
+            Console.WriteLine("Valeur: " + Value.Text);
+        }
+
+        private void onChangeText(object sender, TextChangedEventArgs e)
+        {
+            Console.WriteLine("Changement");
+        }
+
+        public void onValueChange(string valeur)
+        {
+            
+            Console.WriteLine("avant: " + Value.Text);
+            Value.Text = valeur;
+            Console.WriteLine("après: " + Value.Text + "\n");
+        }
+    }
+}
